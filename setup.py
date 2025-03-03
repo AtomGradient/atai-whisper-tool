19d49cd065598189bf3d96af4bfe68254134327f
wushuhong <wushuhong@atomgradient.com>
2025-03-04 02:03:35 +0800

    release:0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="atai-whisper-tool",
-    version="0.0.2",
+    version="0.0.3",
     author="AtomGradient",
     author_email="alex@atomgradient.com",
     description="OpenAI Whisper with Apple MPS support",
